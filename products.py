58d8d1de257eb89eab1ee061b4dffdc64799f01c
Cathy Wei <Cathyweii79@gmail.com>
2020-02-29 10:54:46 +0800

    add check file

--- products.py
+++ products.py
@@ -1,13 +1,21 @@
-#先讀取舊的檔案再存新的, 跳過首欄說明文字
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '購買商品,商品價格' in line:
-			continue
-		name, price = line.strip().split(',') 
-		products.append([name, price])
+import os #operating system
 
-print(products)
+#讀取檔案
+products = []
+if os.path.isfile('products.csv'):    #os系統模組裡的path模組的isfile功能(檢查檔案在不在)
+	print('找到檔案')
+
+	with open('products.csv', 'r', encoding='utf-8') as f:   #先讀取舊的檔案再存新的, 跳過首欄說明文字
+		for line in f:
+			if '購買商品,商品價格' in line:
+				continue  #繼續
+			name, price = line.strip().split(',') 
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案...')
+
 
 #讓使用者輸入資料
 while True:
